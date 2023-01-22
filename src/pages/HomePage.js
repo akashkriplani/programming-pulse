dc7dd701ff5f15008731f26903b56c1a1cf73723
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-22 22:26:38 +0530

    Add a sidebar to load the list of users

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,15 +9,28 @@ const reducer = (state, action) => {
       return { ...state, posts: action.payload, error: '', loading: false };
     case 'POSTS_FAILURE':
       return { ...state, error: action.payload, loading: false };
+    case 'USERS_REQUEST':
+      return { ...state, loadingUsers: true };
+    case 'USERS_SUCCESS':
+      return { ...state, users: action.payload, errorUsers: '', loadingUsers: false };
+    case 'USERS_FAILURE':
+      return { ...state, errorUsers: action.payload, loadingUsers: false };
     default:
       return state;
   }
 };
 
 export default function HomePage() {
-  const [state, dispatch] = useReducer(reducer, { loading: false, error: '', posts: [] });
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: '',
+    posts: [],
+    users: [],
+    loadingUsers: false,
+    errorUsers: ''
+  });
 
-  const { loading, error, posts } = state;
+  const { loading, error, posts, loadingUsers, errorUsers, users } = state;
 
   const loadPosts = async () => {
     dispatch({ type: 'POSTS_REQUEST' });
@@ -30,8 +43,20 @@ export default function HomePage() {
     }
   };
 
+  const loadUsers = async () => {
+    dispatch({ type: 'USERS_REQUEST' });
+
+    try {
+      const { data } = await axios.get('http://jsonplaceholder.typicode.com/users');
+      dispatch({ type: 'USERS_SUCCESS', payload: data });
+    } catch (err) {
+      dispatch({ type: 'USERS_FAILURE', payload: err.message });
+    }
+  };
+
   useEffect(() => {
     loadPosts();
+    loadUsers();
   }, []);
 
   return (
@@ -51,6 +76,22 @@ export default function HomePage() {
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
               </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="sidebar">
+        <h2>Authors</h2>
+        {loadingUsers ? (
+          <div>Loading...</div>
+        ) : errorUsers ? (
+          <div>Error: {errorUsers}</div>
+        ) : users.length === 0 ? (
+          <div>No author found</div>
+        ) : (
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
             ))}
           </ul>
         )}
